1591ef2ff135749394ef48441f15dc45b545e287
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-08-12 10:23:20 -0400

    doc(Liquid.Translators.General): @type/@spec annotations

--- lib/liquid/translators/general.ex
+++ lib/liquid/translators/general.ex
@@ -16,6 +16,7 @@ defmodule Liquid.Translators.General do
 
   """
 
+  @spec variable_in_parts(Liquid.Combinators.LexicalToken.variable_value()) :: String.t()
   def variable_in_parts(variable) do
     Enum.map(variable, fn {key, value} ->
       case key do
@@ -26,6 +27,10 @@ defmodule Liquid.Translators.General do
     end)
   end
 
+  @doc """
+  Remove the `?` symbol from the variable name string
+  """
+  @spec string_have_question(String.t()) :: String.t()
   def string_have_question(value) when is_bitstring(value) do
     if String.contains?(value, "?") do
       String.replace(value, "?", "")
@@ -34,17 +39,31 @@ defmodule Liquid.Translators.General do
     end
   end
 
+  @doc """
+  Returns true when a tuple is an Else/Elseif tag
+  """
+  @spec is_else(tuple()) :: boolean()
   def is_else({:else, _}), do: true
   def is_else({:elsif, _}), do: true
   def is_else(_), do: false
 
-  def not_open_if({key, _}), do: !Enum.member?([:evaluation, :else, :elsif], key)
+  @doc """
+  Returns true when a tuple is and If tag
+  """
+  @spec not_open_if(tuple()) :: boolean()
+  def not_open_if({:evaluation, _}), do: false
+  def not_open_if({:else, _}), do: false
+  def not_open_if({:elsif, _}), do: false
   def not_open_if(_), do: true
 
+  @doc """
+  When element is a list, returns the first value in, else returns element
+  """
+  @spec types_no_list(List.t()) :: String.t() | Number.t()
   def types_no_list([]), do: []
 
   def types_no_list(element) do
-    if is_list(element), do: List.first(element), else: element
+    if is_list(element), do: hd(element), else: element
   end
 
   def types_only_list(element) do
