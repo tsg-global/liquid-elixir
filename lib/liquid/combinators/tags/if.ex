e72c5d1ac4c50862eb9e56f1fd6d2e21d4d1fd14
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-08-16 19:28:55 -0500

    doc(If): document public functions

--- lib/liquid/combinators/tags/if.ex
+++ lib/liquid/combinators/tags/if.ex
@@ -1,7 +1,7 @@
 defmodule Liquid.Combinators.Tags.If do
   @moduledoc """
   Executes a block of code only if a certain condition is true.
-  If this condition is false executes `else` block of code
+  If this condition is false executes `else` block of code.
   Input:
   ```
     {% if product.title == 'Awesome Shoes' %}
@@ -31,6 +31,9 @@ defmodule Liquid.Combinators.Tags.If do
           ]
         ]
 
+  @doc """
+  Parse a `Liquid` Elsif tag.
+  """
   def elsif_tag do
     "elsif"
     |> Tag.open_tag(&predicate/1)
@@ -39,8 +42,14 @@ defmodule Liquid.Combinators.Tags.If do
     |> optional(parsec(:__parse__))
   end
 
+  @doc """
+  Parse a `Liquid` Unless tag.
+  """
   def unless_tag, do: do_tag("unless")
 
+  @doc """
+  Parse a `Liquid` If tag.
+  """
   def tag, do: do_tag("if")
 
   defp body do
@@ -51,6 +60,9 @@ defmodule Liquid.Combinators.Tags.If do
     |> tag(:body)
   end
 
+  @doc """
+  Parse Elsif body.
+  """
   def body_elsif do
     empty()
     |> choice([
