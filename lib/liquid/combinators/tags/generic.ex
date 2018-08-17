9df4faa25b8866b63dd2d49a8752c727efb32e16
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-08-16 19:28:35 -0500

    doc(Generic): document public functions

--- lib/liquid/combinators/tags/generic.ex
+++ lib/liquid/combinators/tags/generic.ex
@@ -1,7 +1,7 @@
 defmodule Liquid.Combinators.Tags.Generic do
   @moduledoc """
-  Secondary tags used inside primary tags
-  We defined a tag as secondary when it needs a primary tag to work
+  Secondary tags used inside primary tags.
+  We defined a tag as secondary when it needs a primary tag to work.
   For example, `else` tag is used by `if`, `for` and `cycle` but id doesn't
   work alone
   """
@@ -9,5 +9,8 @@ defmodule Liquid.Combinators.Tags.Generic do
 
   @type else_tag :: [else: Liquid.NimbleParser.t()]
 
+  @doc """
+  Parse a `Liquid` Else tag.
+  """
   def else_tag, do: Tag.define_inverse_open("else")
 end
