c5052f418fce56b2c5fa0e7f9e7f7a3521f34342
Antonio Abella <antonioabella@gmail.com>
2018-08-27 07:50:30 -0500

    refactor: add Render module to alias

--- lib/liquid/tags/capture.ex
+++ lib/liquid/tags/capture.ex
@@ -10,7 +10,7 @@ defmodule Liquid.Capture do
   ```
   Capture is useful for saving content for use later in your template, such as in a sidebar or footer.
   """
-  alias Liquid.{Block, Template, Context}
+  alias Liquid.{Block, Render, Template, Context}
 
   @doc """
   Implementation of Capture parse operations.
@@ -28,10 +28,10 @@ defmodule Liquid.Capture do
           {list(), %Context{}} | {list(), %Block{}, %Context{}}
   def render(output, %Block{markup: markup, nodelist: content}, %Context{} = context) do
     variable_name = Liquid.variable_parser() |> Regex.run(markup) |> hd
-    {block_output, context} = Liquid.Render.render([], content, context)
+    {block_output, context} = Render.render([], content, context)
 
     result_assign =
-      context.assigns |> Map.put(variable_name, block_output |> Liquid.Render.to_text())
+      context.assigns |> Map.put(variable_name, block_output |> Render.to_text())
 
     context = %{context | assigns: result_assign}
     {output, context}
