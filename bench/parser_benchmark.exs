24f71986aa8756414fe02a6a6bc35499e52d3b4d
Antonio Abella <antonioabella@gmail.com>
2018-09-04 08:30:54 -0500

    feat(benchmarks): run multiple scenarios

--- bench/parser_benchmark.exs
+++ bench/parser_benchmark.exs
@@ -1,38 +1,43 @@
 Liquid.start()
 
-complex_template = File.read!("test/templates/complex/01/input.liquid")
+complex = File.read!("test/templates/complex/01/input.liquid")
 
-middle_template = """
-  {% decrement product.price %}
+middle = """
   <h1>{{ product.name }}</h1>
   <h2>{{ product.price }}</h2>
-  {% increment product.price %}
   <h2>{{ product.price }}</h2>
   {% comment %}This is a commentary{% endcomment %}
   {% raw %}This is a raw tag{% endraw %}
   {% for item in array %} Repeat this {% else %} Array Empty {% endfor %}
 """
 
-simple_template = """
+simple = """
   {% for item in array %} Repeat this {% else %} Array Empty {% endfor %}
 """
 
-empty_template = ""
+empty = ""
 
-template = complex_template
+templates = [complex: complex, middle: middle, simple: simple, empty: empty]
 
 time = DateTime.to_string(DateTime.utc_now())
 
-Benchee.run(
-  %{
-    nimble: fn -> Liquid.NimbleParser.parse(template) end,
-    regex: fn -> Liquid.Parse.parse(template, %Liquid.Template{}) end
-  },
-  warmup: 5,
-  time: 60,
-  formatters: [
-    Benchee.Formatters.Console,
-    Benchee.Formatters.CSV
-  ],
-  formatter_options: [csv: [file: "bench/results/parser-benchmarks-#{time}.csv"]]
+Enum.each(templates,
+  fn {name, template} ->
+    IO.puts "running: #{name}"
+    Benchee.run(
+      %{
+        "#{name}-regex" => fn -> Liquid.Template.old_parse(template) end,
+        "#{name}-nimble-with-translate" => fn -> Liquid.Template.parse(template) end,
+        "#{name}-nimble" => fn -> Liquid.NimbleParser.parse(template) end
+      },
+      warmup: 5,
+      time: 60,
+      formatters: [
+        Benchee.Formatters.Console,
+        Benchee.Formatters.CSV
+      ],
+      formatter_options: [csv: [file: "bench/results/parser-benchmarks-#{time}.csv"]]
+    )
+  end
 )
+
