74037a82a5f34bb01281b67fd6444e493337a205
Antonio Abella <antonioabella@gmail.com>
2018-09-07 07:41:50 -0500

    refactor(Comment): clean legacy parse code

--- lib/liquid/tags/comment.ex
+++ lib/liquid/tags/comment.ex
@@ -1,6 +1,3 @@
 defmodule Liquid.Comment do
-  def parse(%Liquid.Block{} = block, %Liquid.Template{} = template),
-    do: {%{block | blank: true, strict: false}, template}
-
   def render(output, %Liquid.Block{}, context), do: {output, context}
 end
