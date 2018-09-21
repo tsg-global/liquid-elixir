5e3a3bd0b399adea06cc040f84481d3936b8da93
Antonio Abella <antonioabella@gmail.com>
2018-09-20 22:27:11 -0500

    doc: add spec annotations and more explicit docs

--- lib/liquid/tokenizer.ex
+++ lib/liquid/tokenizer.ex
@@ -1,15 +1,16 @@
 defmodule Liquid.Tokenizer do
   @moduledoc """
-  Prepares markup to be parsed
+  Prepares markup to be parsed. Tokenizer splits the code between starting literal and rest of markup.
+  When called recursively, it allows to process only liquid part (tags and variables) and bypass the slower literal.
   """
 
   alias Liquid.Combinators.General
 
   @doc """
   Takes a markup, find start of liquid construction (tag or variable) and returns
-  a tuple with two elements: a literal and a possible liquid construction
+  a tuple with two elements: a literal and rest(with tags/variables and optionally more literals)
   """
-  @spec tokenize(String.t()) :: []
+  @spec tokenize(String.t()) :: {String.t(), String.t()}
   def tokenize(markup) do
     case :binary.match(markup, [
            General.codepoints().start_tag,
