1279944d4ff3846a0a7aeaa0bc8f4341b7ca38d4
Antonio Abella <antonioabella@gmail.com>
2018-09-20 22:23:42 -0500

    doc(AST): add docs to public functions

--- lib/liquid/ast.ex
+++ lib/liquid/ast.ex
@@ -1,11 +1,23 @@
 defmodule Liquid.Ast do
+  @moduledoc """
+  Builds the AST processing with Nimble, only liquid valid tags and variables. It uses Tokenizer
+  to send to Nimble only tags and variables, without literals.
+  Literals (any markup which is not liquid variable or tag) are slow to be processed by Nimble thus
+  this module improve performance between 30% and 100% depending how much text is processed.
+  """
   alias Liquid.{Tokenizer, Parser}
 
-  def build("", context, ast), do: {:ok, ast, context, ""}
+  @doc """
+  Recursively builds the AST taking a markup, or a tuple with a literal and a rest markup.
+  It uses a context to validate the correct opening and close of blocks and sub blocks.
+  """
+  @spec build(String.t() | {String.t(), String.t()}, Keyword.t(), List.t()) ::
+          {:ok, List.t(), Keyword.t(), String.t()} | {:error, String.t(), String.t()}
   def build({literal, ""}, context, ast), do: {:ok, Enum.reverse([literal | ast]), context, ""}
   def build({"", markup}, context, ast), do: process_liquid(markup, context, ast)
   def build({literal, markup}, context, ast), do: process_liquid(markup, context, [literal | ast])
 
+  def build("", context, ast), do: {:ok, ast, context, ""}
   def build(markup, context, ast) do
     markup |> Tokenizer.tokenize() |> build(context, ast)
   end
@@ -26,6 +38,9 @@ defmodule Liquid.Ast do
 
   defp process_liquid(markup, context, ast) do
     case Parser.__parse__(markup, context: context) do
+      {:ok, [{:error, message}], rest, _, _, _} ->
+        {:error, message, rest}
+
       {:ok, [{:end_block, _}], rest, nimble_context, _line, _offset} ->
         {:ok, ast, nimble_context, rest}
 
